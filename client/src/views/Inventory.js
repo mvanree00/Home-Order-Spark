748e3c553dd683fd493f1b53d5526fdcebaf8735
carsonhamel <65917917+carsonhamel@users.noreply.github.com>
2020-07-28 01:21:34 -0400

    UI Standardized

--- client/src/views/Inventory.js
+++ client/src/views/Inventory.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
+import 'fontsource-roboto'
+import Typography from '@material-ui/core/Typography'
 
 const Inventory = (props) => {
     if(props.user.atype === "Volunteer"){
         return (
             <div align="center">
-                <h1>Here are the items you are currently delivering, {props.user.name}!</h1>
-                
+                <Typography variant="h1">Here are the items you are currently delivering, {props.user.name}!</Typography>
             </div>
         )
     }
     else if(props.user.atype === "Store"){
         return (
             <div align="center">
-                <h1>Welcome to your store inventory, {props.user.name}!</h1>
-                <h1>Here are the items you are currently selling</h1>
+                <Typography variant="h1">Welcome to your store inventory, {props.user.name}!</Typography>
+                <Typography variant="h2">Here are the items you are currently selling</Typography>
                 
             </div>
         )
@@ -22,8 +23,8 @@ const Inventory = (props) => {
     else{
         return (
             <div align="center">
-                <h1>Welcome to your Cart, {props.user.name}!</h1>
-                <h1>Items in cart</h1>
+                <Typography variant="h1">Welcome to your Cart, {props.user.name}!</Typography>
+                <Typography variant="h2">Items in cart</Typography>
                 <Button variant ="contained" color="primary" type="submit">Go to checkout</Button>
             </div>
             
