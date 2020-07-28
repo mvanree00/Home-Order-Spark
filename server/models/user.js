c0fffbdfbe0772640717faa62566685a7589d20f
mvanree00 <mvanree00@gmail.com>
2020-07-27 21:05:04 -0400

    fixed all schemas

--- server/models/user.js
+++ server/models/user.js
@@ -1,22 +1,34 @@
 const mongoose = require('mongoose'),
     bcrypt = require('bcrypt-nodejs');
 
+const itemSchema = new mongoose.Schema({
+    itemName: {type: String, required: true, unique: true},
+    description: { type: String, required: true },
+    price: {type: mongoose.Number},
+    quantity: {type: mongoose.Number}
+});
+
+const storeSchema = new mongoose.Schema({
+    storeName: {type: String, required: true},
+    address: {type: String, required: true},
+    items: [itemSchema]
+});
+
+const orderSchema = new mongoose.Schema({
+    status: {type: String, required: true},
+    total: {type: mongoose.Number, required: true},
+    items: [{
+        objid: {type: String}
+    }]
+});
+
 const userSchema = new mongoose.Schema({
     name: {type: String},
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true},
     atype: {type: String, required: true}, // account type (volunteer, store owner, null if customer)
-    store : {
-        storeName: {type: String, required: true},
-        address: {type: String, required: true},
-        items: [{
-            itemName: {type: String, required: true, unique: true},
-            description: { type: String, required: true },
-            price: {type: mongoose.Number},
-            quantity: {type: mongoose.Number}
-        }],
-        required : false
-    }
+    store: storeSchema,
+    orders: [orderSchema]
 });
 
 // adds method to user to create hashed password
