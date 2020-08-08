eac147e50303f8826566b75813f2f7a9be08ebd6
mvanree00 <mvanree00@gmail.com>
2020-08-07 21:34:22 -0400

    prevents adding same item twice

--- server/controllers/carts.js
+++ server/controllers/carts.js
@@ -1,4 +1,5 @@
 const Cart = require('../models/cart.js');
+const Item = require('../models/item.js');
 
 module.exports = {
     index: async (req, res) => {
@@ -11,8 +12,13 @@ module.exports = {
     },
     create: async (req, res) => {
         try{
-            const cart = await Cart.create(req.body);
-            res.json({success: true, message: "Cart created."});
+            if(Item.find({_id: req.body.prodId})){
+                res.json({success: false, message: "Already in cart, alter quantity in cart"})
+            }
+            else{
+                const cart = await Cart.create(req.body);
+                res.json({success: true, message: "Cart created."});
+            }
         } catch(err) {
             res.json({success: false, code: err.code});
         }
